3fd9af8eb6132b2c4956728ad175d1094a9766e7
Arthur M <arthurmccomp@gmail.com>
2026-02-06 22:43:28 +0000

    renamed:    aula1_js/index.html -> aula_JavaScript/aula1_js/index.html 	renamed:    aula1_js/script.js -> aula_JavaScript/aula1_js/script.js 	renamed:    aula2_js/index.html -> aula_JavaScript/aula2_js/index.html 	renamed:    aula2_js/operadores.js -> aula_JavaScript/aula2_js/operadores.js 	renamed:    aula3_js_mario/css/style.css -> aula_JavaScript/aula3_js_mario/css/style.css 	renamed:    aula3_js_mario/images/bullet.png -> aula_JavaScript/aula3_js_mario/images/bullet.png 	renamed:    aula3_js_mario/images/clouds.png -> aula_JavaScript/aula3_js_mario/images/clouds.png 	renamed:    aula3_js_mario/images/game-over.png -> aula_JavaScript/aula3_js_mario/images/game-over.png 	renamed:    aula3_js_mario/images/mario-beginner.gif -> aula_JavaScript/aula3_js_mario/images/mario-beginner.gif 	renamed:    aula3_js_mario/images/mario-flying.gif -> aula_JavaScript/aula3_js_mario/images/mario-flying.gif 	renamed:    aula3_js_mario/images/mario-pro.gif -> aula_JavaScript/aula3_js_mario/images/mario-pro.gif 	renamed:    aula3_js_mario/images/mario-starter.gif -> aula_JavaScript/aula3_js_mario/images/mario-starter.gif 	renamed:    aula3_js_mario/images/mario.gif -> aula_JavaScript/aula3_js_mario/images/mario.gif 	renamed:    aula3_js_mario/images/pipe.png -> aula_JavaScript/aula3_js_mario/images/pipe.png 	renamed:    aula3_js_mario/images/turtle.gif -> aula_JavaScript/aula3_js_mario/images/turtle.gif 	renamed:    aula3_js_mario/index.html -> aula_JavaScript/aula3_js_mario/index.html 	renamed:    aula3_js_mario/js/script.js -> aula_JavaScript/aula3_js_mario/js/script.js 	renamed:    aula3_js_mario/sound/game.mp3 -> aula_JavaScript/aula3_js_mario/sound/game.mp3 	renamed:    aula3_js_mario/sound/jump.wav -> aula_JavaScript/aula3_js_mario/sound/jump.wav 	renamed:    aula3_js_mario/sound/mariodie.wav -> aula_JavaScript/aula3_js_mario/sound/mariodie.wav 	renamed:    aula3_js_mario/sound/powerup.wav -> aula_JavaScript/aula3_js_mario/sound/powerup.wav 	renamed:    aula_4/css/style.css -> aula_JavaScript/aula_4/css/style.css 	renamed:    aula_4/index.html -> aula_JavaScript/aula_4/index.html 	renamed:    aula_4/script.js -> aula_JavaScript/aula_4/script.js 	renamed:    aula_5/index.html -> aula_JavaScript/aula_5/index.html 	renamed:    aula_5/script.js -> aula_JavaScript/aula_5/script.js 	new file:   aula_PHP/aula1_php/index.html 	new file:   aula_PHP/aula1_php/script.php

---
+++ aula_PHP/aula1_php/script.php
@@ -0,0 +1,17 @@
+<?php
+
+$id = 1;
+$title = "Duck";
+$price = 12.99;
+$description = "Um descrição descrevendo como uma descrição seria descrita de acordo com a descrição dada";
+$category = "Pato";
+$image = "isso ae";
+
+echo $id;
+echo $title;
+echo $price;
+echo $description;
+echo $category;
+echo $image;
+
+?>
